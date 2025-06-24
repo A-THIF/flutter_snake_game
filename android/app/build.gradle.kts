4c39b8742fc1aa7aa4ff0c9d828facbc37b4bcad
A-THIF <149256653+A-THIF@users.noreply.github.com>
2025-06-24 17:10:34 +0530

    Made black background and with landscape only with screen not sleep
    Integrated the wakelock_plus package to keep the screen awake
    Updated the app to launch in immersive landscape mode.
    Simplified the main widget to display a black screen
    Updated Android NDK version and

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,8 +8,7 @@ plugins {
 android {
     namespace = "com.example.snake_game_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
+    ndkVersion = "27.0.12077973" // Add or modify this line
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
