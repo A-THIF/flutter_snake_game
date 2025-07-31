cd33d0ecd0955cc72c21ca42acbee96b60116fb5
A-THIF <149256653+A-THIF@users.noreply.github.com>
2025-07-31 11:56:03 +0530

    WIP: local edits to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:snake_game_app/controls.dart';
+import 'package:snake_game_app/splash_screen.dart';
 import 'package:wakelock_plus/wakelock_plus.dart';
 
 void main() async {
-  // Sets up Flutter's internal system.
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Force landscape mode (like classic snake game).
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.landscapeLeft,
     DeviceOrientation.landscapeRight,
   ]);
 
-  // Hides top status bar and bottom navigation.
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
 
-  // Keeps the screen ON (useful for games).
   await WakelockPlus.enable();
 
-  // Launch the game screen
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: GameController(),
+      home: SplashScreen(), // ✅ Start with splash!
     ),
   );
 }
